df746ff4fb3514673df336272cab37d1ec18cc0f
Marco Esposito <marco.esposito@tum.de>
2015-09-22 21:24:12 +0200

    handeye_calibration publisher can send inverse transform of calibration

--- handeye_calibration/scripts/publisher.py
+++ handeye_calibration/scripts/publisher.py
@@ -3,12 +3,15 @@
 __author__ = 'marco'
 
 import rospy
-from tf import TransformBroadcaster
+from tf import TransformBroadcaster, TransformerROS, transformations as tfs
+from geometry_msgs.msg import Transform
 
 rospy.init_node('handeye_calibration_publisher')
-while rospy.get_time() == 0.0: pass
+while rospy.get_time() == 0.0:
+    pass
 
 eye_on_hand = rospy.get_param('eye_on_hand')
+inverse = rospy.get_param('inverse')
 
 orig = None
 dest = None 
@@ -30,12 +33,22 @@ rotation = (rospy.get_param(prefix + '_qx'),
             rospy.get_param(prefix + '_qz'),
             rospy.get_param(prefix + '_qw'))
 
-rospy.loginfo('publishing transformation ' + orig + ' -> ' + dest + ':\n' + str((translation,rotation)))
+if inverse:
+    transformer = TransformerROS()
+    result_tf = Transform(translation, rotation)
+    cal_mat = transformer.fromTranslationRotation(result_tf.translation, result_tf.rotation)
+    cal_mat_inv = tfs.inverse_matrix(cal_mat)
+    translation = tfs.translation_from_matrix(cal_mat_inv)
+    rotation = tfs.quaternion_from_matrix(cal_mat_inv)
+
+    orig, dest = dest, orig
+
+rospy.loginfo('publishing transformation ' + orig + ' -> ' + dest + ':\n' + str((translation, rotation)))
 
 broad = TransformBroadcaster()
 
 rate = rospy.Rate(50)
 
 while not rospy.is_shutdown():
-    broad.sendTransform(translation, rotation, rospy.Time.now(), dest, orig)  #  FIXME: looks like inverted
+    broad.sendTransform(translation, rotation, rospy.Time.now(), dest, orig)
     rate.sleep()
