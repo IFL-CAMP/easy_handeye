5bdea628bcd4692d38d723464bf91f25ca14a6fd
Marco Esposito <marco.esposito@tum.de>
2015-12-02 02:08:12 +0100

    Working on handeye_calibration_commander

--- handeye_calibration/scripts/handeye_calibration_commander.py
+++ handeye_calibration/scripts/handeye_calibration_commander.py
@@ -8,17 +8,17 @@ import std_srvs
 import handeye_calibration as hec
 
 
-# for reading single character without hitting RETURN
+# for reading single character without hitting RETURN (unless it's ipython!)
 def getchar():
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
 
 class HandeyeCalibrationCommander:
@@ -29,21 +29,23 @@ class HandeyeCalibrationCommander:
         self.save_calibration_proxy = rospy.ServiceProxy(hec.SAVE_CALIBRATION_TOPIC, std_srvs.srv.Empty)
 
     def _edit_menu(self):
-        sample_to_delete=None
+        sample_to_delete = None
         while sample_to_delete != '':
             prompt_str = 'Press a number and ENTER to delete the respective sample, or ENTER to continue:\n'
             for i in range(len(self.samples)):
                 prompt_str += str(i + 1) + ' ' + str(self.samples[i]) + '\n'
             sample_to_delete = raw_input(prompt_str)
             if sample_to_delete.isdigit():
-                del self.samples[int(sample_to_delete)-1]
+                self.remove_sample_proxy(sample_to_delete)
 
     def _save_menu(self):
-        i = getchar('Press c to compute the calibration or ENTER to continue\n')
+        print('Press c to compute the calibration or ENTER to continue\n')
+        i = getchar()
         if i == 'c':
-            cal = self.command_compute_calibration()
+            cal = self.compute_calibration_proxy()
             print(cal)
-        i = raw_input('Press q+ENTER to quit or ENTER to continue\n')
+        print('Press q+ENTER to quit or ENTER to continue\n')
+        i = getchar()
         if i == 'q':
             quit()
 
@@ -52,14 +54,8 @@ class HandeyeCalibrationCommander:
         self._save_menu()
 
     def spin_interactive(self):
-        rospy.loginfo('Base link frame: ' + self.base_link_frame)
-        rospy.loginfo('End effector frame: ' + self.tool_frame)
-        rospy.loginfo('Optical origin frame: ' + self.optical_origin_frame)
-        rospy.loginfo('Optical target frame: ' + self.optical_target_frame)
-        rospy.loginfo('Waiting for transforms between frames')
-        self._wait_for_tf_init()
         while not rospy.is_shutdown():
-             self._interactive_menu()
+            self._interactive_menu()
 
 
 def main():
@@ -67,8 +63,8 @@ def main():
     while rospy.get_time() == 0.0:
         pass
 
-
-    # TODO: command line interface
+    cmder = HandeyeCalibrationCommander()
+    cmder.spin_interactive()
 
 
 if __name__ == '__main__':
